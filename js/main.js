97a11f4510f0758adfec1f2a59fb45c696422220
Julianfloresd <106127778+julianfloresd@users.noreply.github.com>
2023-09-01 16:15:32 -0400

    Basic layout created

--- js/main.js
+++ js/main.js
@@ -1,31 +1,10 @@
-const findTheOldest = function(array) {
-  const date = new Date()
-  let currentYear = date.getFullYear
-  
-  array.forEach(element => {
-      if (element.hasOwnProperty("yearOfDeath") == true) {
-        element.ageCalc = element.yearOfDeath - element.yearOfBirth
-      } else {
-        element.ageCalc = currentYear - element.yearOfBirth
-      }
-    
-  });
+const myLibrary= [];
 
-  let highestAge = 0;
+function Book () {
+  // constructor
+}
 
-  for (let i = 0; i < array.length - 1; i++) {
-    if (array[i].ageCalc > highestAge) {
-      highestAge = array[i].ageCalc
-    } else {
-      continue;
-    }
-  }
 
-  for (let n = 0; n < array.length - 1; n++) {
-    if (array[n].ageCalc == highestAge) {
-      return array[n]
-    } else {
-      continue;
-    }
-  }
-};
+function addBookToLibrary () {
+  // do stuff
+}
