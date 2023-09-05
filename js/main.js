1979d0445ab387ec35cee0b0f7fdde756fcead95
Julianfloresd <106127778+julianfloresd@users.noreply.github.com>
2023-09-05 07:15:40 -0400

    added Book construct & addBookToLibrary functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
+
+
 const myLibrary= [];
 
-function Book () {
-  // constructor
+function Book (title, author, pages, read) {
+  this.bookTitle = title;
+  this.bookAuthor = author;
+  this.bookPages = pages;
+  this.bookSummary = summary;
+  this.read = read;
+  this.info = function() {
+    let sentence = ''
+    if (read == 'yes') {
+      sentence = `${title},by ${author} has ${pages} pages and I have read it.`
+    } else {
+      sentence = sentence = `${title},by ${author} has ${pages} pages and I have not read it.`
+    }
+    console.log(sentence);
+    return sentence
+  }
 }
 
 
-function addBookToLibrary () {
-  // do stuff
+function addBookToLibrary (item) {
+  const bookShelf = document.querySelector('.cardContainer')
+  let newBook = document.createElement('div');
+  let newTitle = document.createElement('h2')
+  let newAuthor = document.createElement('h3')
+  let newPages = document.createElement('h3')
+  newBook.classList.add('bookCard');
+  bookShelf.appendChild(newBook)
+  newBook.appendChild(newTitle)
+  newTitle.textContent = `${item.bookTitle}`;
+  newBook.appendChild(newAuthor)
+  newAuthor.textContent = `by: ${item.bookAuthor}`;
+  newBook.appendChild(newPages)
+  newPages.textContent = `# of Pages: ${item.bookPages}`;
 }
